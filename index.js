6672ccec987596ac1041749b2c495b168adb60b9
emreyoleri-gitHub <emreyoleri01@hotmail.com>
2021-12-06 21:43:59 +0300

    Register & Login Users

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const resolvers = require("./graphql/resolvers");
+
+const typeDefs = require("./graphql/typeDefs");
+
+require("dotenv").config();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDb Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
